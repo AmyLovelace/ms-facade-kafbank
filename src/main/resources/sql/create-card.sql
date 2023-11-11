8992ec3b0658f8e2b410ac48c21c875283c27136
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-11 00:34:36 -0300

    prueba compilacion arreglé init.sql

--- src/main/resources/sql/create-card.sql
+++ src/main/resources/sql/create-card.sql
@@ -1,8 +1,18 @@
+CREATE OR REPLACE FUNCTION create_card()
+RETURNS TRIGGER AS $$
+BEGIN
+  INSERT INTO card (account_id, card_number, membership, description_status, balance)
+  VALUES (NEW.id, generate_random_card_number(), 'Standard', 'Activo', 0);
+
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
 CREATE TABLE IF NOT EXISTS card (
-    id SERIAL PRIMARY KEY,
-    account_id INT NOT NULL REFERENCES account (id),
-    card_number VARCHAR(255) NOT NULL UNIQUE,
-    membership VARCHAR(255) NOT NULL,
-    description_status VARCHAR(255) NOT NULL,
-    balance INT NOT NULL
+  id SERIAL PRIMARY KEY,
+  account_id INT NOT NULL,
+  card_number VARCHAR(255) NOT NULL UNIQUE,
+  membership VARCHAR(255) NOT NULL,
+  description_status VARCHAR(255) NOT NULL,
+  balance INT NOT NULL
 );
