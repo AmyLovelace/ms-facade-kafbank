38a423b9acbd3c933a5698a49d3009f421712964
Amanecer Cabrera <amanecer.luz.cabrera@gmail.com>
2023-11-10 23:24:26 -0300

    Update CardJDBCAdapter.java

--- src/main/java/com/ppj/acl/adapter/jdbc/postgres/CardJDBCAdapter.java
+++ src/main/java/com/ppj/acl/adapter/jdbc/postgres/CardJDBCAdapter.java
@@ -1,5 +1,6 @@
 package com.ppj.acl.adapter.jdbc.postgres;
 
+import com.ppj.acl.adapter.jdbc.dao.sql.GenericDAO;
 import com.ppj.acl.application.port.out.CardJDBCRepository;
 import com.ppj.acl.domain.Card;
 import lombok.extern.slf4j.Slf4j;
@@ -13,7 +14,23 @@ public class CardJDBCAdapter implements CardJDBCRepository {
 
     private static final String SQL_INSERT_ACCOUNT = "sql/insert-card.sql";
 
+    private static final String KEY_CARD_NUM = "cardnumber";
+    private static final String KEY_MEMBERSHIP = "membership";
+    private static final String KEY_CARD_STATUS = "cardstatus";
+    private static final String KEY_DESCRIPTION = "descriptionstatus";
 
+    private static final String KEY_BALANCE = "balance";
+
+    private final GenericDAO dao;
+
+    private final String createCard;
+    private final String insertCard;
+
+    public CardJDBCAdapter(GenericDAO dao, String createCard, String insertCard) {
+        this.dao = dao;
+        this.createCard = createCard;
+        this.insertCard = insertCard;
+    }
 
     @Override
     public String CardCreate(Card card) {
